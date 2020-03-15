ad96da795697da17aff80eeae3c2acddcacabee8
Fuzz <writefayaz@gmail.com>
2020-03-15 11:15:54 +0400

    Get Index

--- MinHeap.playground/Contents.swift
+++ MinHeap.playground/Contents.swift
@@ -3,4 +3,20 @@ import Foundation
 struct MinHeap{
     var item:[Int] = []
     
+    //MARK: Get left child index
+    private func getLeftChildIndex(_ parentIndex: Int) -> Int {
+        return 2 * parentIndex + 1
+    }
+    
+    //MARK: Get right child index
+    private func getRightChildIndex(_ parentIndex: Int) -> Int {
+        return 2 * parentIndex + 2
+    }
+    
+    //MARK: Get parent index
+    private func getParentIndex(_ childIndex: Int) -> Int {
+        return ( childIndex - 1 ) / 2
+    }
+    
+    
 }
